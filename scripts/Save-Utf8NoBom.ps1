44f3068351760800c576cc1e3ffcad5b1c540ffd
Anuraj Deol <AnurajDeol90@gmail.com>
2025-10-14 17:38:45 -0500

    build: add Save-Utf8NoBom.ps1 helper to avoid BOM on Windows

---
+++ scripts/Save-Utf8NoBom.ps1
@@ -0,0 +1,6 @@
+﻿param(
+  [Parameter(Mandatory=$true)][string]$Path,
+  [Parameter(Mandatory=$true)][string]$Content
+)
+$utf8NoBom = New-Object System.Text.UTF8Encoding($false)
+[System.IO.File]::WriteAllText((Resolve-Path $Path), $Content, $utf8NoBom)
